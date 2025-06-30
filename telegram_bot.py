b896aaf717042ea9e3ed90138e7a48bab8a0242e
Shyama Tripathi <115423865+shyamatripathi@users.noreply.github.com>
2025-06-30 17:38:18 +0530

    Add files via upload

---
+++ telegram_bot.py
@@ -0,0 +1,26 @@
+
+import os
+import django
+
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship_project.settings')
+django.setup()
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from core.models import TelegramUser
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    username = update.message.from_user.username
+    chat_id = update.message.chat_id
+
+    TelegramUser.objects.get_or_create(username=username, chat_id=chat_id)
+    await update.message.reply_text(f"Hello @{username}, you're registered!")
+
+def main():
+    app = ApplicationBuilder().token('7999326010:AAFeQtHWSBBcc2qNOV7D5d-QrsdTK_L5J-U').build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
